422ca24a68b03fbeea58800af297f3190b801c12
Rafael Carlos Soriano <sorianomarmol@gmail.com>
2019-07-26 14:26:30 +0200

    Pruebas graficas con fusionchart

--- exo_currency/exchange_rates/urls.py
+++ exo_currency/exchange_rates/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('', views.exchange_rate_evolution, name='exchange_rate_evolution'),
-    path('rates/base/(?P<base>[a-zA-Z]{3})/months/(?P<months>\d+)', views.exchange_rate_evolution, name='index_with_params')
+    # path('rates/base/(?P<base>[a-zA-Z]{3})/months/(?P<months>\d+)', views.exchange_rate_evolution, name='index_with_params')
 ]
